de172f892b2c16197ba1fbc2db3e717b3780b4e1
rigwild <contact@asauvage.fr>
2018-11-30 10:59:39 +0100

    dbconfig

---
+++ models/DatabaseConfig.class.php
@@ -0,0 +1,12 @@
+<?php
+class Config {
+  static public $DatabaseCredentials = [
+    'SGBD'     => 'mysql',
+    'host'     => 'localhost',
+    'port'     => '3306',
+    'dbName'   => 'zchat',
+    'username' => 'zchat',
+    'password' => 'qDib9(DAu2r4g?AVmf88KZ-4Y)/%2@^LhD!mC3>57&5ttiK3#9bTH@h~86jH9>'
+  ];
+}
+?>
