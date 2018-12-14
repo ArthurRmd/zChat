5376754adae6ff9c5938f3a2cdf6200abb6de46f
rigwild <contact@asauvage.fr>
2018-12-14 15:56:07 +0100

    emoji

--- api/lib/Util.class.php
+++ api/lib/Util.class.php
@@ -5,7 +5,7 @@ class Util {
   public static function checkLoggedInAPI()
   {
     if (!(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'])) {
-      http_response_code(401);
+      http_response_code(403);
       exit();
     }
   }
@@ -24,10 +24,30 @@ class Util {
     return json_last_error() === JSON_ERROR_NONE ? $json : null;
   }
 
+  // Convert :emojis: to unicode emojis
   public static function convertEmoji($str = "")
   {
+    // Get emojis
     $emojis = file_get_contents(__DIR__.'/discord_emojis.min.json');
     $emojis = json_decode($emojis, true);
+
+    // Match possible emojis in string
+    $matched = [];
+    preg_match_all('/\:\w+\:/', $str, $matched);
+    $matched = $matched[0];
+
+    // Get the emojis for the matches
+    $matchedEmojis = [];
+    foreach ($matched as $key => $value) {
+      $search = array_key_exists($value, $emojis);
+      if ($search) $matchedEmojis[$value] = $emojis[$value];
+    }
+
+    // Replace emojis in the string
+    foreach ($matchedEmojis as $key => $value)
+      $str = str_replace($key, $value, $str);
+
+    return $str;
   }
 }
 
