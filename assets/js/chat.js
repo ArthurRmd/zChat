b4e5e786a52af66b612d31ff47fc905a044360d9
ArthurRmd <43524062+ArthurRmd@users.noreply.github.com>
2018-12-19 23:08:39 +0100

    Front-end : Sidebar, input, bulle chat

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -10,11 +10,38 @@ Vue.config.devtools = true;
 
 $(document).ready(function () {
   $(".btn").click(function () {
+
+
     console.log("clique")
-    $('.sidebar')
-      .sidebar('setting', 'transition', 'overlay')
-      .sidebar('toggle');
-  });
+
+
+    $('.sidebar').sidebar('setting', {
+      dimPage             : false,
+      closable            : false,
+      transition          : 'overlay',
+      mobileTransition    : 'overlay'})
+    .sidebar('toggle');
+
+    
+
+
+    if(!$('.detect').hasClass('visible')){
+      $( ".test" ).addClass( "espace-gauche" );
+      $(".zone-texte").css('width','calc(100vw - 30px - 260px)')
+      $(".zone-texte").css('left','270px')
+    }
+    else{
+      $( ".test" ).removeClass( "espace-gauche");
+      $(".zone-texte").css('width','calc(100vw - 30px)')
+      $(".zone-texte").css('left','10px')
+    }
+
+
+    });
+
+    
+
+
 })
 
 
