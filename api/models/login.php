93d8ab067d7423f63abe73e220ef00a9d235ad61
rigwild <contact@asauvage.fr>
2018-12-19 21:01:16 +0100

    Fix login

--- api/models/login.php
+++ api/models/login.php
@@ -16,13 +16,28 @@ try {
     $httpCode = 409;
     return;
   }
+
   $res = $res[0];
+
+  if ($res['is_banned']) {
+    // Error : User is banned
+    $error = "You are banned from the website.";
+    $httpCode = 403;
+    return;
+  }
+  
   if (!password_verify($password, $res['password'])) {
     // Error : Bad password
     $error = "Invalid username or password";
     $httpCode = 409;
     return;
   }
+
+  // We remove useless infos for $_SESSION
+  $res = [
+    'id' => $res['id'],
+    'pseudo' => $res['pseudo']
+  ];
 } catch (PDOException $e) {
   $error = $e->getMessage();
 }
