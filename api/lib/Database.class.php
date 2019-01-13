7d4b6b53f9fcbbe08463a54599f3a83e05092c36
rigwild <contact@asauvage.fr>
2019-01-13 11:51:45 +0100

    Modified database class

--- api/lib/Database.class.php
+++ api/lib/Database.class.php
@@ -1,38 +1,52 @@
 <?php
 
+require_once 'DatabaseConfig.class.php';
+
 /**
+ * Easily communicate with your database.
+ * 
  * Usage : 
  * $dbLink = new Database();
  * 
- * $req1 = $dbLink->execute('SELECT * FROM users');
- * $req2 = $dbLink->execute('SELECT * FROM users WHERE username = :username', ['username' => 'rigwild']);
+ * $req1 = $dbLink->select('SELECT * FROM users');
+ * $req2 = $dbLink->select('SELECT * FROM users WHERE username = :username', ['username' => 'rigwild']);
+ * $req3 = $dbLink->execute('INSERT INTO users VALUES (:username), ['username' => 'rigwild']);
  * 
- * The link with the database is established once on each reload.
+ * The link with the database is established only once to save performance.
+ * 
+ * MySQL and PostgreSQL are supported.
  * 
  * @author rigwild - https://github.com/rigwild
  * @see https://gist.github.com/rigwild/5d4660f3f6f979171496a11e548020d9
  */
-
-require_once 'DatabaseConfig.class.php';
-
 class Database {
-  private $dbCredentials = null;
+  private $dbConfig = null;
   private $connection = null;
 
   public function __construct() {
-    $this->dbCredentials = Config::$DatabaseCredentials;
+    $this->dbConfig = DatabaseConfig::$Config;
   }
 
-  private function connect() {
+  /**
+   * Connect to the database and backup the PDO object for next use.
+   * Public so you can get the PDO object if you need it.
+   * 
+   * @return PDO pdo connection object
+   */
+  public function connect() {
     if ($this->connection !== null)
       return $this->connection;
-    $dbInfos = $this->dbCredentials['SGBD']
-      .':host='.$this->dbCredentials['host']
-      .';port='.$this->dbCredentials['port']
-      .';dbname='.$this->dbCredentials['dbName']
-      .';charset=utf8mb4';
-    $username = $this->dbCredentials['username'];
-    $password = $this->dbCredentials['password'];
+    $dbInfos = $this->dbConfig['SGBD']
+      .':host='.$this->dbConfig['host']
+      .';port='.$this->dbConfig['port']
+      .';dbname='.$this->dbConfig['dbName'];
+
+    if ($this->dbConfig['SGBD'] === 'mysql') $dbInfos .= ';charset=utf8mb4';
+    elseif ($this->dbConfig['SGBD'] === 'pgsql') $dbInfos .= ';options=\'-c client_encoding=utf8\'';
+
+    $username = $this->dbConfig['username'];
+    $password = $this->dbConfig['password'];
+
     $conn = new PDO($dbInfos, $username, $password);
     $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -46,7 +60,7 @@ class Database {
    * @param query the SQL query to execute
    * @param parametersArray an array of parameters
    * 
-   * @return array the result of the query
+   * @return boolean the result of the query
    */
   public function select($query, $parametersArray = []) {
     $conn = $this->connect();
@@ -66,7 +80,8 @@ class Database {
   public function execute($query, $parametersArray = []) {
     $conn = $this->connect();
     $stmt = $conn->prepare($query);
-    return ($stmt->execute($parametersArray));
+    return $stmt->execute($parametersArray);
   }
 }
+
 ?>
--- api/lib/DatabaseConfig.default.class.php
+++ api/lib/DatabaseConfig.default.class.php
@@ -2,19 +2,19 @@
 
 /**
  * Configure the connection to the database.
+ * SGBD can be 'mysql' or 'pgsql'. Change Database.class.php if you need another driver.
  * 
  * @author rigwild - https://github.com/rigwild
  * @see https://gist.github.com/rigwild/5d4660f3f6f979171496a11e548020d9
  */
-
-class Config {
-  static public $DatabaseCredentials = [
+class DatabaseConfig {
+  static public $Config = [
     'SGBD'     => 'mysql',
     'host'     => 'localhost',
-    'port'     => '3306',
-    'dbName'   => 'zchat',
-    'username' => 'zchat',
-    'password' => 'qDib9(DAu2r4g?AVmf88KZ-4Y)/%2@^LhD!mC3>57&5ttiK3#9bTH@h~86jH9>'
+    'port'     => 3306,
+    'dbName'   => '',
+    'username' => '',
+    'password' => ''
   ];
 }
 ?>
